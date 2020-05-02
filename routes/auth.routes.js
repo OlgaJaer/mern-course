5e26175179e6be1115e793d88bb5de815a9a38fd
Olga Jaeger <olgar.mamaeva@rambler.ru>
2020-05-02 19:01:14 +0200

    add login route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,51 +1,89 @@
 const { Router } = require("express");
-const bcrypt = require('bcryptjs')
-const { check, validationResult} = require("express-validator")
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
 const router = Router();
-const User = require('../models/User')
+const config = require("config");
+const User = require("../models/User");
 
 // /api/auth/register
-router.post("/register", 
-[
-    check('email', 'Некорректный емайл').isEmail(),
-    check('password', "Минимальная длина пароля не менее 6 символов").isLength({min: 6})
-],
-async (req, res) => {
-try {
-    const errors = validationResult(req)
+router.post(
+  "/register",
+  [
+    check("email", "Некорректный емайл").isEmail(),
+    check("password", "Минимальная длина пароля не менее 6 символов").isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
+      if (!errors.isEmpty()) {
         return res.status(400).json({
-            errors: errors.array(),
-            message: "Некорруктные данные при регистрации"
-        })
+          errors: errors.array(),
+          message: "Некорректные данные при регистрации",
+        });
+      }
+
+      const { email, password } = req.body;
+      const condidate = await User.findOne({ email }); // email: email
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Что-то пошло не так, попробуйте снова" });
     }
-
-    const { email, password} = req.body 
-    const condidate = await User.findOne({email}) // email: email
-
-    if (candidate) {
-        return res.status(400).json({message: "Такой пользователь уже существует"}) 
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 12)
-    const user = new User({email, password: hashedPassword})
-
-    await user.save()
-    res.status(201).json('Пользователь создан') 
-
-} catch (error) {
-    res.status(500).json({message: "Что-то пошло не такб попробуйте снова"})
-}
-});
+  }
+);
 
 // /api/auth/login
-router.post("/login", async (req, res) => {
+router.post(
+  "/login",
+  [
+    check("email", "Введите корректный емайл").normalizeEmail().isEmail(),
+    check("password", "Введите пароль").exists(),
+  ],
+  async (req, res) => {
     try {
-        
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: "Некорректные данные при входе в систему",
+        });
+      }
+
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ message: "Такой пользователь не существует" });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ message: "Неверный пароль,попробуйте снова" });
+      }
+
+      const token = jwt.sign(
+          { userId: user.id }, 
+        config.get("jwtSecret"),
+        {expiresIn: '1h'}
+      );
+
+      res.json({token, userId: user.id})   // по умолчанию статус 200
     } catch (error) {
-        
+      res
+        .status(500)
+        .json({ message: "Что-то пошло не так, попробуйте снова" });
     }
-});
+  }
+);
 
 module.exports = router;
