0c4a93985b4fbd78776a0fde55310a038430a380
Olga Jaeger <olgar.mamaeva@rambler.ru>
2020-05-08 15:14:04 +0200

    create component LinksPage, LinksList

--- client/src/pages/LinksPage.js
+++ client/src/pages/LinksPage.js
@@ -1,9 +1,34 @@
-import  React from 'react'
+import React, { useState, useContext, useCallback, useEffect } from "react";
+import { useHttp } from "../hooks/http.hook";
+import { AuthContext } from "../context/AuthContext";
+import { Loader } from "../components/Loader";
+import {LinksList} from "../components/LinksList"
 
-export const LinksPage = ()=> {
-    return (
-        <div>
-            <h1>Links Page</h1>
-        </div>
-    )
-}
+export const LinksPage = () => {
+  const { token } = useContext(AuthContext);
+  const { request, loading } = useHttp();
+  const [links, setLinks] = useState([]);
+
+  const fetchLinks = useCallback(async () => {
+    try {
+      const fetched = await request(`/api/link`, "GET", null, {
+        Authorization: `Bearer ${token}`,
+      });
+      setLinks(fetched)
+    } catch (error) {}
+  }, [token, request]);
+
+  useEffect(() => {
+    fetchLinks();
+  }, [fetchLinks]);
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  return (
+    <>
+    {!loading && <LinksList links={links} />}
+    </>
+  );
+};
