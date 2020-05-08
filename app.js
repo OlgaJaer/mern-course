190e2e44ef4e159453b06bb44df9ff7ed9744f16
Olga Jaeger <olgar.mamaeva@rambler.ru>
2020-05-08 16:15:05 +0200

    prepare deploy

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const config = require("config");
 const mongoose = require("mongoose");
 
@@ -8,7 +9,15 @@ app.use(express.json({ extended: true }));
 
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/link", require("./routes/links.routes"));
-app.use('/t', require('./routes/redirect.routes'))
+app.use("/t", require("./routes/redirect.routes"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "client", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = config.get("port") || 5000;
 
